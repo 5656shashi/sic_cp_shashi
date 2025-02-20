d9adff2210d90c756c646fbc850cac894a117ce9
5656shashi <5656shashidhar@gmail.com>
2025-02-20 09:27:52 +0530

    changes

--- day3/db2.py
+++ day3/db2.py
@@ -1,29 +1,53 @@
 import pymysql
 def connect_db():
     try:
-        connection = pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='shashi_db',charset='utf8')
-        print("DB connected")
-        return connection   
+        connection=pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='shashi_db',charset='utf8')
+        print('DB Connected')
     except:
-        print("DB connected failed")
+        print('DB Connection failed')
+    return connection
 
 def disconnect_db(connection):
     try:
         connection.close()
-        print("DB disconnected")
+        print('DB Disconnecetd')
     except:
-        print("Error in disconnection")
+        print("DB Disconnection failed")
 
 def create_db():
-    connection = connect_db()
-    query = 'create database IF NOT EXISTS shashi_db;'
-    cursor = connection.cursor()
+    connection=connect_db()
+    query='create database IF NOT EXISTS shashi_db'
+    cursor=connection.cursor()
     cursor.execute(query)
+    cursor.close()
     disconnect_db(connection)
 
-def insert_row():
+def create_table():
     connection=connect_db()
-c=connect_db()
-if c:
-    disconnect_db(c)
+    query="""create table IF NOT EXISTS persons(id int primary key auto_increment, name varchar(32) not null, gender char check(gender in('m','M', 'f','F')), location varchar(32), dob DATETIME);"""
+    cursor = connection.cursor()
+    cursor.execute(query)
+    cursor.close()
+    print('Table created')
+    disconnect_db(connection)
+
+def read_person_details():
+    name = input('Enter person name: ')
+    gender = input('Enter person gender: ')
+    location = input('Enter person location: ')
+    dob = input('Enter person date of borth(yyyy-mm-dd): ')
+    return (name, gender, location, dob)
+
+def insert_row():
+    person = read_person_details()
+    query = 'insert into persons(name, gender, location, dob) values(%s, %s, %s, %s);'
+    connection = connect_db()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute(query, person)
+        connection.commit()
+        cursor.close()
+        disconnect_db(connection)
 create_db()
+create_table()
+insert_row()
